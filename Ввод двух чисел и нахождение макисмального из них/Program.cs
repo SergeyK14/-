d35adee40c2758a0b3ab551421ba00f6235332aa
SergeyK <serega_konchakov@mail.ru>
2022-11-15 22:56:55 +0300

    Ввод двух чисел и нахождение максимального из них

---
+++ Ввод двух чисел и нахождение макисмального из них/Program.cs
@@ -0,0 +1,11 @@
+﻿// Ввод двух чисел и нахождение максbмального из них
+Console.WriteLine ("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+if (number2 > max)
+{
+    Console.WriteLine("Максимальное число равно " + number2);
+}
+else Console.WriteLine("Максимальное число равно " + max);
