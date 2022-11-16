3e0c5e46403910b01d5ee070a6b3be0d2001179c
SergeyK <serega_konchakov@mail.ru>
2022-11-16 23:40:35 +0300

    Проверка введенного числа на четность

---
+++ Определение, является ли число четное/Program.cs
@@ -0,0 +1,8 @@
+﻿    //Определение, является ли введенное число четным
+    Console.WriteLine ("Введите число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    if (number%2 == 0)
+    {
+        Console.WriteLine("Число является четным");
+    }
+    else {Console.WriteLine("Число является нечетным");}
