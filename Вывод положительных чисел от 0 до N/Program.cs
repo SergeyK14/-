b60124cd80c32e51bf6792fba593f263b4e0f6bd
SergeyK <serega_konchakov@mail.ru>
2022-11-18 21:08:47 +0300

    Вывод положительных числе от 0 до N

---
+++ Вывод положительных чисел от 0 до N/Program.cs
@@ -0,0 +1,19 @@
+﻿    //Определение и вывод положительных чисел до числа N, введенного пользователем
+    Console.WriteLine ("Введите число: ");
+    int N = Convert.ToInt32(Console.ReadLine());
+    int number = 0;
+    int number1 = 0;
+    if (N < 0)
+    {
+        Console.WriteLine("Введите положительное число ");
+    }             
+    else
+    {  
+        while (number <= N)
+        {
+            if (number%2 == 0)
+            number1 = number;
+            Console.WriteLine(number1);
+            number = number + 2;
+        }
+    }
